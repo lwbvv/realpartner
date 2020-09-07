904b25e2ffdc8024e1c1870e2aae6112795b4160
Leechiwon <chiwon.lee@appg.co.kr>
2020-09-07 09:54:00 +0900

    Start Project

---
+++ realpartner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realpartner
+//
+//  Created by Developer Appg on 2020/09/07.
+//  Copyright © 2020 appg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
